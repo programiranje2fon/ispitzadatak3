6b90c4233a19b08645d137480032014cc244b214
sevarac <sevarac@gmail.com>
2019-01-22 21:50:37 +0100

    ociscena ispravka

--- zadatak3ispravka/src/test/Test.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import ispravka_koda.UcitavanjeSaTastature3;
-
-public class Test {
-	public static void main(String[] args) {
-		UcitavanjeSaTastature3.ucitajIProveriNivenov();
-	}
-}
